9ac416813a6857746d028506538587c33eca2be9
Olie Santos <oliesantosfreelance@gmail.com>
2024-08-27 20:58:32 +0800

    INITIAL COMMIT

---
+++ src/main/java/com/recime/recipe/RecipeMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.recime.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeMicroserviceApplication.class, args);
+	}
+
+}
